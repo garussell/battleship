a9658f29e5f365bc885189f62574bf4b446a524e
Allen Russell <garyrusselljr@gmail.com>
2023-05-25 17:02:16 -0600

    feat: add hit method

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,17 +1,19 @@
 class Ship
   attr_reader :name,
-              :length
+              :length,
+              :health
 
   def initialize(name, length)
     @name = name
     @length = length
-  end
-
-  def health
-    @length
+    @health = length
   end
 
   def sunk?
     @health == 0
+  end
+
+  def hit
+    @health -= 1
   end
 end

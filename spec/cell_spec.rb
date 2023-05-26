6ca32b6e075fa2869a62e33c02f0d2fb7246a9d7
Allen Russell <garyrusselljr@gmail.com>
2023-05-26 15:05:31 -0600

    test: add render and render(arg) method tests

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -16,6 +16,8 @@ RSpec.describe Cell do
       expect(@cell_1.empty?).to eq(true)
     end
   end
+
+  describe "#ship empty?" do
     it 'can place a ship in a cell' do
       
       @cell_1.place_ship(@cruiser)
@@ -23,7 +25,9 @@ RSpec.describe Cell do
       expect(@cell_1.ship).to eq(@cruiser)
       expect(@cell_1.empty?).to eq(false)
     end
+  end
 
+  describe "#fire_upon" do
     it 'can be fired upon' do
       @cell_1.place_ship(@cruiser)
 
@@ -34,4 +38,23 @@ RSpec.describe Cell do
       expect(@cell_1.ship.health).to eq(2)
       expect(@cell_1.fired_upon?).to be(true)
     end
+  end
+
+  describe "#render" do
+# hide the ship
+    it 'can render cells as string to print board' do
+      expect(@cell_1.render).to eq(".")
+      @cell_1.fire_upon
+      expect(@cell_1.render).to eq("M")
+
+      @cell_2.place_ship(@cruiser)
+      expect(@cell_2.render).to eq(".")
+    end
+# show the ship
+    it 'it can render with optional argument' do
+      expect(@cell_2.render(true)).to eq("S")
+      @cell_2.fire_upon
+      expect(@cell_2.render).to eq("H")
+    end
+  end
 end

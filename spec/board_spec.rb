4e6f4e7a01229a45b1539e71a691db4aa1519999
Garrett Varenhorst <garrettvarenhorst@gmail.com>
2023-05-27 10:45:25 -0500

    test: Add and pass 'can recognize valid coordinates' test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 RSpec.describe Cell do
   before(:each) do
     @board = Board.new
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
   end
 
   describe '#initialize' do 
@@ -16,4 +18,21 @@ RSpec.describe Cell do
       expect(@board.cells["A4"].coordinate).to eq("A4")
     end
   end
+  describe '#validations' do
+    it 'can recognize valid coordinates' do
+
+      expect(@board.valid_coordinate?("A1")).to eq(true)
+      expect(@board.valid_coordinate?("D4")).to eq(true)
+      expect(@board.valid_coordinate?("A5")).to eq(false)
+      expect(@board.valid_coordinate?("E1")).to eq(false)
+      expect(@board.valid_coordinate?("A22")).to eq(false)
+    end
+
+    it 'can validate placements' do
+
+      expect(@board.valid_placement?(@cruiser, ["A1", "A2"])).to eq(false)
+      expect(@board.valid_placement?(@submarine, ["A2", "A3", "A4"]).to eq(false)
+    end
+  end
+
 end

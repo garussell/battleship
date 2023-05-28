9b1bbcea6b45e7b08e6ba35fe348ffcaf62d502c
Garrett Varenhorst <garrettvarenhorst@gmail.com>
2023-05-28 11:23:14 -0500

    test: Add and pass #validations tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -31,7 +31,11 @@ RSpec.describe Cell do
     it 'can validate placements' do
 
       expect(@board.valid_placement?(@cruiser, ["A1", "A2"])).to eq(false)
-      expect(@board.valid_placement?(@submarine, ["A2", "A3", "A4"]).to eq(false)
+      expect(@board.valid_placement?(@submarine, ["A2", "A3", "A4"])).to eq(false)
+      expect(@board.valid_placement?(@cruiser, ["A1", "A2", "A4"])).to eq(false)
+      expect(@board.valid_placement?(@submarine, ["A1", "C1"])).to eq(false)
+      expect(@board.valid_placement?(@cruiser, ["A3", "A2", "A1"])).to eq(false)
+      expect(@board.valid_placement?(@submarine, ["C1", "B1"])).to eq(false)
     end
   end
 

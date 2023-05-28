2b52916300119089c53b8196688f76f12980a1a7
Allen Russell <garyrusselljr@gmail.com>
2023-05-28 11:44:57 -0600

    fix: Clear out pseudo-code for clean main branch

--- lib/board.rb
+++ lib/board.rb
@@ -51,25 +51,3 @@ class Board
     end
   end
 end
-
-# def placement_length
-#   ship.length == coordinates.length
-# end
-# @board.cells.ship.length(?) - 
-# does conseceutive_order method need if (block) else nil to elimate diagonals?
-# what if the length == the coordinates range?
-# placement must be equal to ship size "return false if 'placement'!= @length"
-# placement must be either horizontal or vertical, not diagnal
-# how to make sure there are no gaps: enumerable .each_cons(n){ ... }   ?
-#   horizontal =  - current_placement = A1 ... current_placement.next = A2 ... if current placement != next than false
-# vertical = current = A1, next = B1 ... or, if @cruiser && A1, than A2/A3 or B1/C1
-
-### helper methods:
-## hypothesis for consecutive_order helper method:
-# so valid_placement? takes two args (ship, coordinates) ... coordinates is an array that we can iterate through.  maybe we can use .ord to convert A1/A2 into numbers then sort them.  If sorted value != original value than return false... aka not valid placement
-
-## hypothesis for placement_length:
-# coordinates.length == ship.length
-
-##hypothesis for vertical || horizontal placement:
-# vertical || horizontal therefore  not diagonal
